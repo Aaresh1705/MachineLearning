6f4f986822bd61cb13b43f3e5bfa5a1000c870c9
Marucs Z. Olssen <marcus.olssen@gmail.com>
2025-04-12 18:38:08 +0200

    Tested the fully connected neural network, and got it to work

--- test.py
+++ test.py
@@ -1,29 +1,54 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import time
+import mycelium as net
 
+if __name__ == '__main__':
+    net.seed(42)
 
-def test_matrix_mult():
-    N = 10000
-    input_dim = 10
-    output_dims = np.linspace(1, 1000, 100,  dtype=int)
-    time_for_dim_py = []
-    time_for_dim_npy = []
-    for output_dim in output_dims:
-        w = np.random.randn(output_dim, input_dim)
-        b = np.random.randn(output_dim)
-        a = np.random.uniform(-5, 5, (N, input_dim))
-        start = time.time()
-        for _ in a:
-            w @ _ + b
-        end = time.time()
-        time_for_dim_py.append(end - start)
-        start = time.time()
-        for _ in a:
-            np.dot(w, _) + b
-        end = time.time()
-        time_for_dim_npy.append(end - start)
-    plt.plot(output_dims, time_for_dim_py, 'blue')
-    plt.plot(output_dims, time_for_dim_npy, 'red')
-    plt.legend(['Python matrix multiplication', 'Numpy matrix multiplication'])
+    X, y = net.data.sin_data()
+
+    Relu = net.activation.Relu
+    Sin = net.activation.Sin
+
+    dense1 = net.layers.Input(1)
+    dense2 = net.layers.Dense(500, Relu)
+    dense3 = net.layers.Dense(1)
+
+    dense2.initializeWeights(dense1.dim)
+    dense3.initializeWeights(dense2.dim)
+
+    loss_function = net.losses.MSE()
+    optimizer = net.optimizers.Adam()
+    accuracy = net.accuracy.MAE()
+
+    for epoch in range(10001):
+        hidden1 = dense1.forward(X)
+        hidden2 = dense2.forward(hidden1)
+        hidden3 = dense3.forward(hidden2)
+
+        data_loss = loss_function.forward(hidden3, y)
+        acc = accuracy.forward(hidden3, y)
+
+        if not epoch % 100:
+            print(f'epoch: {epoch}, ' +
+                  f'acc: {acc:.3f}, ' +
+                  f'loss: {data_loss:.3f} (' +
+                  f'lr: {optimizer.current_learning_rate}')
+
+        d_value3 = loss_function.backward(hidden3, y)
+        d_value2 = dense3.backward(d_value3)
+        d_value1 = dense2.backward(d_value2)
+
+        optimizer.pre_update_params()
+        optimizer.update(dense2)
+        optimizer.update(dense3)
+        optimizer.post_update_params()
+
+    import matplotlib.pyplot as plt
+
+    hidden1 = dense1.forward(X)
+    hidden2 = dense2.forward(hidden1)
+    hidden3 = dense3.forward(hidden2)
+
+    plt.plot(X, y, label='data')
+    plt.plot(X, hidden3, label='model')
+    plt.legend()
     plt.show()
